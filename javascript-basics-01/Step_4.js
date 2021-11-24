3e33c6aef32ca6d6eefe04a536b0960a7c28c04d
Omair <abd.alkhahhar@gmail.com>
2021-11-24 14:47:05 +0200

    JS-basics-01-step-4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+let name = window.prompt("enter your name please");
+let surname = window.prompt("enter your surname please");
+let city = window.prompt("enter your city please");
+alert("Hello "+ name + " " + surname + " from " + city);
