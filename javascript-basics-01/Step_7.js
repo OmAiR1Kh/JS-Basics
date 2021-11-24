b012359d7fdf558164ea9e230232fc4b8678fc90
Omair <abd.alkhahhar@gmail.com>
2021-11-24 14:57:41 +0200

    JS-basics-01-step-7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,3 @@
+let shoe = window.prompt("enter your shoe size please");
+let year = window.prompt("enter your birth year please")
+alert(`your result is ${((((shoe * 2) + 5) * 50) - year) + 1776}`)
