c294a34a52bf717675b20b77e82ff05b9ef5c587
Omair <abd.alkhahhar@gmail.com>
2021-11-24 15:34:44 +0200

    JS-basics-02-step 1

---
+++ javascript-basics-02/step 1/index.js
@@ -0,0 +1,4 @@
+document.querySelector("#image1")
+document.getElementById("image1").addEventListener('mouseover', function(){
+    document.getElementById("image1").src = "./images/image1_2.jpg"
+})
